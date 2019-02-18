81367c80db495893dd6128db2a4e22c011ec480c
QuixoteHY <1290482442@qq.com>
2019-02-18 09:08:05 +0800

    注释debug代码

--- test/tornardo/cookies.py
+++ test/tornardo/cookies.py
@@ -25,73 +25,73 @@ check_dict = {
 
 
 class BaseHandler(tornado.web.RequestHandler):
-    def prepare(self):
-        print('|||||prepare: ')
-
-    def on_finish(self):
-        print('|||||finish: ', self.request.cookies)
-
+    # def prepare(self):
+    #     print('|||||prepare: ')
+    #
+    # def on_finish(self):
+    #     print('|||||finish: ', self.request.cookies)
+    #
     def get_current_user(self):
         temp = self.get_secure_cookie("username")
         print('get_current_user: '+str(temp))
         return temp
-
-    def clear_cookie(self, name, path="/", domain=None):
-        print('----- name=', name)
-        expires = datetime.datetime.utcnow() - datetime.timedelta(days=365)
-        self.set_cookie(name, value="", path=path, expires=expires, domain=domain)
-
-    def clear_all_cookies(self, path="/", domain=None):
-        print('-'*30+' clear_all_cookies')
-        print('-', self.request.cookies)
-        for name in self.request.cookies:
-            self.clear_cookie(name, path=path, domain=domain)
-        print('-'*30+' clear_all_cookies')
-
-    def set_secure_cookie(self, name, value, expires_days=30, version=None, **kwargs):
-        print('-'*30+' set_secure_cookie')
-        print('- ', name, ' = ', value)
-        self.set_cookie(name, self.create_signed_value(name, value, version=version),
-                        expires_days=expires_days, **kwargs)
-        print('-'*30+' set_secure_cookie')
-
-    def check_xsrf_cookie(self):
-        token = (self.get_argument("_xsrf", None) or
-                 self.request.headers.get("X-Xsrftoken") or
-                 self.request.headers.get("X-Csrftoken"))
-        print('token: '+str(token))
-        if not token:
-            raise HTTPError(403, "'_xsrf' argument missing from POST")
-        _v, token, _t = self._decode_xsrf_token(token)
-        print(_v, 'token: '+str(token), _t)
-        _v, expected_token, _t = self._get_raw_xsrf_token()
-        print(_v, 'expected_token: '+str(expected_token), _t)
-        if not token:
-            raise HTTPError(403, "'_xsrf' argument has invalid format")
-        if not _time_independent_equals(utf8(token), utf8(expected_token)):
-            raise HTTPError(403, "XSRF cookie does not match POST argument")
-
-    def _get_raw_xsrf_token(self):
-        print('*'*30+' _get_raw_xsrf_token')
-        print('*RequestHandler ID: '+str(id(self)))
-        print('*', self.request.cookies)
-        if not hasattr(self, '_raw_xsrf_token'):
-            cookie = self.get_cookie("_xsrf")
-            print('*', cookie)
-            if cookie:
-                version, token, timestamp = self._decode_xsrf_token(cookie)
-                print('*Y: ', version, token, timestamp)
-            else:
-                version, token, timestamp = None, None, None
-                print('*N: ', version, token, timestamp)
-            if token is None:
-                version = None
-                token = os.urandom(16)
-                timestamp = time.time()
-                print('*New: ', version, token, timestamp)
-            self._raw_xsrf_token = (version, token, timestamp)
-        print('*'*30+' _get_raw_xsrf_token')
-        return self._raw_xsrf_token
+    #
+    # def clear_cookie(self, name, path="/", domain=None):
+    #     print('----- name=', name)
+    #     expires = datetime.datetime.utcnow() - datetime.timedelta(days=365)
+    #     self.set_cookie(name, value="", path=path, expires=expires, domain=domain)
+    #
+    # def clear_all_cookies(self, path="/", domain=None):
+    #     print('-'*30+' clear_all_cookies')
+    #     print('-', self.request.cookies)
+    #     for name in self.request.cookies:
+    #         self.clear_cookie(name, path=path, domain=domain)
+    #     print('-'*30+' clear_all_cookies')
+    #
+    # def set_secure_cookie(self, name, value, expires_days=30, version=None, **kwargs):
+    #     print('-'*30+' set_secure_cookie')
+    #     print('- ', name, ' = ', value)
+    #     self.set_cookie(name, self.create_signed_value(name, value, version=version),
+    #                     expires_days=expires_days, **kwargs)
+    #     print('-'*30+' set_secure_cookie')
+    #
+    # def check_xsrf_cookie(self):
+    #     token = (self.get_argument("_xsrf", None) or
+    #              self.request.headers.get("X-Xsrftoken") or
+    #              self.request.headers.get("X-Csrftoken"))
+    #     print('token: '+str(token))
+    #     if not token:
+    #         raise HTTPError(403, "'_xsrf' argument missing from POST")
+    #     _v, token, _t = self._decode_xsrf_token(token)
+    #     print(_v, 'token: '+str(token), _t)
+    #     _v, expected_token, _t = self._get_raw_xsrf_token()
+    #     print(_v, 'expected_token: '+str(expected_token), _t)
+    #     if not token:
+    #         raise HTTPError(403, "'_xsrf' argument has invalid format")
+    #     if not _time_independent_equals(utf8(token), utf8(expected_token)):
+    #         raise HTTPError(403, "XSRF cookie does not match POST argument")
+    #
+    # def _get_raw_xsrf_token(self):
+    #     print('*'*30+' _get_raw_xsrf_token')
+    #     print('*RequestHandler ID: '+str(id(self)))
+    #     print('*', self.request.cookies)
+    #     if not hasattr(self, '_raw_xsrf_token'):
+    #         cookie = self.get_cookie("_xsrf")
+    #         print('*', cookie)
+    #         if cookie:
+    #             version, token, timestamp = self._decode_xsrf_token(cookie)
+    #             print('*Y: ', version, token, timestamp)
+    #         else:
+    #             version, token, timestamp = None, None, None
+    #             print('*N: ', version, token, timestamp)
+    #         if token is None:
+    #             version = None
+    #             token = os.urandom(16)
+    #             timestamp = time.time()
+    #             print('*New: ', version, token, timestamp)
+    #         self._raw_xsrf_token = (version, token, timestamp)
+    #     print('*'*30+' _get_raw_xsrf_token')
+    #     return self._raw_xsrf_token
 
 
 class LoginHandler(BaseHandler):
